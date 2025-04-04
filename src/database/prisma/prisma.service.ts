baab691e7aecb1d2c5f66f01c7746106c731859e
Enilton Rodrigues de Andrade <erodri@br.ibm.com>
2025-04-04 17:35:33 -0300

    log: improve Prisma logging

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
@@ -6,9 +11,48 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private readonly logger = new Logger('DATABASE')
   constructor() {
     super({
-      log: ['warn', 'error', 'warn'],
+      log: [
+        { level: 'query', emit: 'event' },
+        { level: 'error', emit: 'event' },
+        { level: 'warn', emit: 'event' },
+      ],
+    })
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('query', (event) => {
+      this.logger.debug(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        `SQL: ${event.query}`,
+      )
+      this.logger.debug(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        `Params: ${event.params}`,
+      )
+      this.logger.verbose(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        `Execution time: ${event.duration}ms`,
+      )
+    })
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('error', (event) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      this.logger.error(`Error: ${event.message}`)
+    })
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('warn', (event) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      this.logger.warn(`Warning: ${event.message}`)
     })
   }
 
